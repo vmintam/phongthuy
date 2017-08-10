ce509399b1122dcd03f37dc94fa32654e510bd1b
vmintam <vmintam@gmail.com>
2017-08-10 16:33:02 +0700

    adding campain

---
+++ campain/campain.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	log "phongthuy/utilities/logging"
+	"strings"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/gin-gonic/gin"
+)
+
+type ServerConf struct {
+	Binding string `toml:"bind"`
+}
+
+type MiscConf struct {
+	RedirectTime int64  `toml:"redirect_time"`
+	BaseURL      string `toml:"base_url"`
+}
+
+type BGConf struct {
+	Server ServerConf `toml:"server"`
+	Misc   MiscConf   `toml:"misc"`
+}
+
+var (
+	conf BGConf
+)
+
+const (
+	CAMPAIN   = `/campain`
+	SPID      = `001011`
+	CPID      = `001011`
+	USERNAME  = `phongthuynguhanh`
+	PASSWORD  = `phongthuy@123p`
+	REGTYPE   = "1"
+	SHOTCODE  = "9432"
+	RETURNURL = "http://phongthuynguhanh.com.vn/callback_authentication"
+	LAYOUT    = "2006-01-02 15:04:01" // phai la 2006-01-02, ko duoc 2006-02-01
+	POSTFIX   = `spid=%s&cpid=%s&transaction_id=%s&return_url=%s&pkg_code=%s&type=%s&shortcode=%s&hashstring=%s`
+)
+
+func GenerateMd5(str string) string {
+	data := []byte(str)
+	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func GenerateSHA256(str string) string {
+	data := []byte(str)
+	return fmt.Sprintf("%x", sha256.Sum256(data))
+}
+
+func init() {
+	//innitial config file
+
+	if len(os.Args) < 2 {
+		print("Not enough parameters \n")
+		os.Exit(1)
+	}
+	if _, err := toml.DecodeFile(os.Args[1], &conf); err != nil {
+		print(err.Error())
+		os.Exit(1)
+	}
+	log.Info("%+v", conf)
+	//Init mysql
+
+}
+
+func post(url string, data []byte) (body []byte, err error) {
+	log.Info("URL %s", url)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	timeout := time.Duration(30 * time.Second)
+	client := &http.Client{
+		Timeout:   timeout,
+		Transport: tr,
+	}
+	r, _ := http.NewRequest("POST", url, bytes.NewBuffer(data)) // <-- URL-encoded payload
+	r.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(r)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("%v", err)
+		return
+	}
+	return
+
+}
+
+func CallBack_Handler(ctx *gin.Context) {
+	msisdn := ctx.Request.Header.Get("msisdn")
+	log.Info("%s", msisdn)
+	source := ctx.DefaultQuery("source", "ptnh")
+	log.Info("%s", source)
+	pkg_code := strings.ToUpper(ctx.DefaultQuery("pkg_code", "VIP"))
+	transaction_id := fmt.Sprintf("%d", time.Now().UnixNano())
+	inEncode := SPID + CPID + transaction_id + pkg_code + RETURNURL + REGTYPE + SHOTCODE + USERNAME + GenerateMd5(GenerateMd5(PASSWORD))
+	inEncode = fmt.Sprintf("%s%d", inEncode, len(inEncode))
+	hashstring := GenerateSHA256(inEncode)
+
+	//log all
+	redirect := conf.Misc.BaseURL + fmt.Sprintf(POSTFIX, SPID, CPID, transaction_id, RETURNURL, pkg_code, REGTYPE, SHOTCODE, hashstring)
+	log.Info("redirect link %s", redirect)
+	// check type is register or unregister
+	ctx.Redirect(http.StatusMovedPermanently, redirect)
+
+}
+
+func main() {
+
+	//go-gin
+	g := gin.Default()
+	g.GET(CAMPAIN, CallBack_Handler)
+	g.Run(conf.Server.Binding)
+	log.Info("go-gin server start...")
+
+}
